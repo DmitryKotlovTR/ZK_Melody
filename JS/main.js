f21cb8fb6626fc577f8479a413785d3f64500dfd
DmitryKotlovTR <dmtrykotlovtr@gmail.com>
2021-08-20 17:17:42 +0300

    Почти сделано

--- JS/main.js
+++ JS/main.js
@@ -2,12 +2,20 @@ $(document).ready(function () {
     var currentFloor = 2;
     var floorPath = $(".home-image path");
     var counterUp = $('.counter-up');
-    var counterDown = $('.counter-down')
+    var counterDown = $('.counter-down');
+    var modal = $('.modal');
+    var modalCloseButton = $('.modal-close-button');
+    var vievFlatsButton = $('.viev-flats');
+
     floorPath.on('mouseover', function() {
         floorPath.removeClass('current-floor');
         currentFloor = $(this).attr('data-floor');
         $('.counter').text(currentFloor);
     });
+
+    floorPath.on('click', toggleModal);
+    modalCloseButton.on('click', toggleModal);
+    vievFlatsButton.on('click', toggleModal);
 
     counterUp.on('click', function() {
         if (currentFloor < 18) {
@@ -28,4 +36,8 @@ $(document).ready(function () {
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
         }
     });
+
+    function toggleModal() {
+        modal.toggleClass(`is-open`);
+    }
 });

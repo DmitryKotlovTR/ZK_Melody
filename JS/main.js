d289bab18a9ff67c57bfb416a1c68b8b5d421011
DmitryKotlov <78270779+DmitryKotlovTR@users.noreply.github.com>
2021-12-24 16:26:49 +0300

    Update main.js
    Доделал жс

--- JS/main.js
+++ JS/main.js
@@ -1,43 +1,74 @@
-$(document).ready(function () {
-    var currentFloor = 2;
-    var floorPath = $(".home-image path");
-    var counterUp = $('.counter-up');
-    var counterDown = $('.counter-down');
-    var modal = $('.modal');
-    var modalCloseButton = $('.modal-close-button');
-    var vievFlatsButton = $('.viev-flats');
+'use strict';
 
-    floorPath.on('mouseover', function() {
-        floorPath.removeClass('current-floor');
-        currentFloor = $(this).attr('data-floor');
-        $('.counter').text(currentFloor);
+$(document).ready(function() {
+  let currentFloor = 2
+  const floorPath = $('.home-image path');
+  const counterUp = $('.counter-up');
+  const counterDown = $('.counter-down');
+  const modal = $('.modal');
+  const modalCloseButton = $('.modal-close-button');
+  const viewFlats = $('.view-flats');
+  const flatsPath = $('.modal-image path');
+  const flatsLink = $('.flat-link');
+  const menuButton = $('.menu-button');
+  const navbarPenal = $('.navbar-penal');
+
+  menuButton.on('click', function() {
+    navbarPenal.toggle('medium');
+  });
+
+  floorPath.on('mouseover', function() {
+    floorPath.removeClass('current-floor');
+    currentFloor = $(this).attr('data-floor');
+    $('.counter').text(currentFloor);
+  });
+
+  function toggleModal() {
+    modal.toggleClass('is-open');
+  }
+
+  floorPath.on('click', toggleModal);
+  modalCloseButton.on('click', toggleModal);
+  viewFlats.on('click', toggleModal);
+
+  function usCounterFloor(floor) {
+    return floor.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
     });
+  }
 
-    floorPath.on('click', toggleModal);
-    modalCloseButton.on('click', toggleModal);
-    vievFlatsButton.on('click', toggleModal);
+  function floorToggle(floor) {
+    $('.counter').text(usCounterFloor(floor));
+    floorPath.removeClass('current-floor');
+    $(`[data-floor="${usCounterFloor(floor)}"]`).toggleClass('current-floor');
+  };
 
-    counterUp.on('click', function() {
-        if (currentFloor < 18) {
-            currentFloor++;
-            usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-            $('.counter').text(usCurrentFloor);
-            floorPath.removeClass('current-floor');
-            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
-        }
-    });
-
-    counterDown.on('click', function() {
-        if (currentFloor > 2) {
-            currentFloor--;
-            usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-            $('.counter').text(usCurrentFloor);
-            floorPath.removeClass('current-floor');
-            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
-        }
-    });
-
-    function toggleModal() {
-        modal.toggleClass(`is-open`);
+  counterUp.on('click', function() {
+    if (currentFloor < 18) {
+      currentFloor++;
+      floorToggle(currentFloor);
     }
+  });
+  counterDown.on('click', function() {
+    if (currentFloor > 2) {
+      currentFloor--;
+      floorToggle(currentFloor);
+    }
+  });
+
+  function deleteClass() {
+    flatsPath.removeClass('current-flat');
+    flatsLink.removeClass('current-flat');
+  }
+
+  flatsPath.on('mouseover', function() {
+    deleteClass();
+    $(`[data-flat-link="${$(this).attr('data-flat')}"]`).toggleClass('current-flat');
+  });
+
+  flatsLink.on('mouseover', function() {
+    deleteClass();
+    $(`[data-flat="${$(this).attr('data-flat-link')}"]`).toggleClass('current-flat');
+  });
 });
